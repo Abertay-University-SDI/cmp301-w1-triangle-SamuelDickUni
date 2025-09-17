78a07290740bd4573d5b3e3cbc287713d9c8569f
2308693 <2308693@MC12931>
2025-09-17 14:55:37 +0100

    Week1 done

--- E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_ps.hlsl
@@ -3,12 +3,15 @@
 
 struct InputType
 {
-	float4 position : SV_POSITION;
-	float4 colour : COLOR;
+    float4 position : SV_POSITION;
+    float2 uv : TEXCOORD;
 };
 
 
 float4 main(InputType input) : SV_TARGET
 {
-	return input.colour;
+    
+    float len = length(input.uv * 2. - 1.);
+    float val = 1. - step(0.5, len);
+    return float4(val,val, val, 1.);
 }
